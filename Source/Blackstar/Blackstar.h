e7b0e0a5f63fa21c84b0bd445a4ba523d03ada3e
Luke Gebbink <lukegebbink@gmail.com>
2020-05-14 12:06:04 -0600

    Update Blackstar.h

--- Source/Blackstar/Blackstar.h
+++ Source/Blackstar/Blackstar.h
@@ -3,3 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#define SURFACE_WEAKCONCRETE		SurfaceType1
+#define SURFACE_WEAKWOOD			SurfaceType2
+#define SURFACE_WEAKMETAL			SurfaceType3
+#define SURFACE_GLASS				SurfaceType4
+#define SURFACE_CONCRETE			SurfaceType5
+#define SURFACE_WOOD				SurfaceType6
+#define SURFACE_METAL				SurfaceType7
+#define SURFACE_FLESHDEFAULT		SurfaceType8
+#define SURFACE_FLESHHEAD			SurfaceType9
+#define SURFACE_FLESHVITALS			SurfaceType10
+#define SURFACE_TV					SurfaceType11
+#define SURFACE_PAPER				SurfaceType12
+#define SURFACE_RECORDPLAYER		SurfaceType13
+#define SURFACE_COP					SurfaceType14
+#define SURFACE_RIOTSHIELD			SurfaceType15
+#define SURFACE_TIRE				SurfaceType16
+
+
+
+
+
+
